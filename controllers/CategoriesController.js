74295a758f1905eb48be98dd20f4b812b0d7b73b
VictorLCosta <victorlc2019@outlook.com>
2021-02-18 22:49:21 -0300

    Category register and Slugify

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../models/Category');
+const slugify = require('slugify');
 
 router.get('/admin/categories/register', (req, res) => 
 {
     res.render('admin/categories/register')
+});
+
+router.post('/categories/save', (req, res) => 
+{
+    var title = req.body.title;
+    if(title != undefined)
+    {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/');
+        });
+    }
+    else
+    {
+        res.redirect('/admin/categories/register');
+    }
 });
 
 module.exports = router;
