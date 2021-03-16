3f4ff0b262db30e815787e24d37930931e08deea
VictorLCosta <victorlc2019@outlook.com>
2021-03-16 17:47:05 -0300

    User save action with password hashing

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt')
 
 router.get('/admin/users', (req, res) => 
 {
@@ -18,7 +19,18 @@ router.post('/admin/users/save', (req, res) =>
     var userName = req.body.userName;
     var password = req.body.password;
 
-    
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email: email,
+        userName: userName,
+        password: hash
+    }).then(() => {
+        res.redirect('/');
+    }).catch((err) => {
+        res.redirect('/admin/users/create');
+    });
 });
 
 module.exports = router;
